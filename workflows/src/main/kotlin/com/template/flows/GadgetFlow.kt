772538202642678e5e7384f219e5eb94becec2c1
Madhuresh Gupta <madhuresh97@yahoo.co.in>
2019-11-13 16:35:34 +0530

    Madhuresh: final edits for flow done

--- workflows/src/main/kotlin/com/template/flows/GadgetFlow.kt
+++ workflows/src/main/kotlin/com/template/flows/GadgetFlow.kt
@@ -16,6 +16,7 @@ import java.util.*
 
 @InitiatingFlow
 @StartableByRPC
+// createProductFlow
 class CompanyAInitiator(
         val to: AbstractParty,
         val from: AbstractParty,
@@ -61,6 +62,62 @@ class CompanyAResponder(val counterpartySession: FlowSession) : FlowLogic<Signed
             override fun checkTransaction(stx: SignedTransaction) = requireThat {
                 val output = stx.tx.outputs.single().data
                 "The output must be a GadgetState" using (output is GadgetState)
+            }
+        }
+        val txWeJustSignedId = subFlow(signedTransactionFlow)
+        return subFlow(ReceiveFinalityFlow(counterpartySession, txWeJustSignedId.id))
+    }
+}
+
+// For Company B to A
+// UpdateProductFlow
+@InitiatingFlow
+@StartableByRPC
+class CompanyBInitiator(
+        val to: AbstractParty,
+        val from: AbstractParty,
+        val productID: UUID,
+        val productName: String,
+        val productColour: String,
+        val Status: String
+) : FlowLogic<SignedTransaction>() {
+    @Suspendable
+    override fun call(): SignedTransaction {
+
+        val notary = serviceHub.networkMapCache.notaryIdentities.first()
+        val command = Command(GadgetContract.Commands.Issue(), listOf(to, from).map { it.owningKey })
+        val gadgetState = GadgetState(
+                to,
+                from,
+                productID,
+                productName,
+                productColour,
+                Status//,
+                //  UniqueIdentifier()
+        )
+
+        val txBuilder = TransactionBuilder(notary)
+                .addOutputState(gadgetState, GadgetContract.ID)
+                .addCommand(command)
+
+        txBuilder.verify(serviceHub)
+        val tx = serviceHub.signInitialTransaction(txBuilder)
+
+        val sessions = (gadgetState.participants - ourIdentity).map { initiateFlow(it as Party) }   //Takes both the companies in list
+        val stx = subFlow(CollectSignaturesFlow(tx, sessions))          // collects signatures of both companies A and B
+        return subFlow(FinalityFlow(stx, sessions))     // Finalizes the transaction to the notary
+    }
+}
+
+@InitiatedBy(CompanyBInitiator::class)
+class CompanyBResponder(val counterpartySession: FlowSession) : FlowLogic<SignedTransaction>() {
+
+    @Suspendable
+    override fun call(): SignedTransaction {
+        val signedTransactionFlow = object : SignTransactionFlow(counterpartySession) {
+            override fun checkTransaction(stx: SignedTransaction) = requireThat {
+                val output = stx.tx.outputs.single().data
+                "The Product has been received" using (output is GadgetState)
             }
         }
         val txWeJustSignedId = subFlow(signedTransactionFlow)
